61abb690bedacf9255ffdf1882607b1e972f98d6
Will <fifyrio@qq.com>
2024-05-30 15:20:53 +0800

    init

--- demo/src/app/components/navbar.tsx
+++ demo/src/app/components/navbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Link from "next/link";
+import Image from 'next/image'
+import Logo from './kiwicode_logo.png'
 
 export default function Navbar() {
   return (
     <nav>
-        <h1>Nav bar</h1>
+        <h1>Hello KiwiCode</h1>
+        <Image
+          src={Logo}
+          width={44}
+          height={44}
+          alt="Picture of the author"
+        />
         <Link href="/">Home</Link>
         <Link href="/tickets">Tickets</Link>
     </nav>
--- demo/src/app/layout.tsx
+++ demo/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
+import { Reddit_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Reddit_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
