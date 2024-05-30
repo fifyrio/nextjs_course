9e17ffc232b71b89b40af9d08c7b0843ac8ed40e
Will <fifyrio@qq.com>
2024-05-30 15:25:55 +0800

    rever to lesson1

--- demo/src/app/layout.tsx
+++ demo/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">      
-      <body className={inter.className}>
-        <Navbar></Navbar>
+      <body className={inter.className}>        
         {children}
         </body>
     </html>
